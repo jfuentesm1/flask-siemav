8a060bc538454cad05964bbcf48afc7ef7c08e0f
jfuentesm1 <jorgefuentesmonserrate@gmail.com>
2024-01-25 01:37:59 -0500

    se añadio instrucciones para ejecutarlo en otra microsd

--- buscador.py
+++ buscador.py
@@ -25,25 +25,29 @@ def listado_componetes(reparacion):
             return identificador
     return False
 
+excel_registro = r'/home/pi/FlaskApp/inventario reparaciones/registro.xlsx'
+excel_componentes = r'/home/pi/FlaskApp/inventario reparaciones/componetes usados.xlsx'
+
 def escritura_excel(sector:str, clase:str ,recibidas:int, reparadas:int, descartadas:int, componetes,cantidad_componentes:int, usuario, direccion):
 
     fecha = datetime.now().strftime("%Y-%m-%d")
+
     try:
         df = pd.read_excel(direccion)
     except FileNotFoundError:
         df = pd.DataFrame(columns=['fecha','camaronera','clase','recibidas','reparadas','descartadas','responsable'])
 
-    if direccion == r'/home/pi/FlaskApp/inventario reparaciones/registro.xlsx':   
+    if direccion == excel_registro:   
         nueva_fila = {'fecha' :fecha, 'camaronera': sector, 'clase':clase, 'recibidas': recibidas, 'reparadas': reparadas, 'descartadas':descartadas,'responsable': usuario}
         df = df.append(nueva_fila, ignore_index=True)
 
-    elif direccion == r'/home/pi/FlaskApp/inventario reparaciones/componetes usados.xlsx':
+    elif direccion == excel_componentes:
         componente_necesario = listado_componetes(componetes)
         nueva_fila = {'fecha': fecha,'componentes':componente_necesario,'cantidad':int(cantidad_componentes), 'responsable': usuario}
         df = df.append(nueva_fila, ignore_index=True)
 
     df.to_excel(direccion, index=False)
-    print(df)
+    #print(df)
     return df
 
 def filtrar_registros(año,mes,dia):
@@ -51,13 +55,13 @@ def filtrar_registros(año,mes,dia):
     fecha = f'{año}-{mes:02d}-{dia:02d}'
 
     try:
-        df1 = pd.read_excel(r"/home/pi/FlaskApp/inventario reparaciones/registro.xlsx")
+        df1 = pd.read_excel(excel_registro)
         df1 = df1[df1['fecha'] == fecha]
     except KeyError:
         df1 = pd.DataFrame()
 
     try:
-        df2 = pd.read_excel(r"/home/pi/FlaskApp/inventario reparaciones/componetes usados.xlsx")
+        df2 = pd.read_excel(excel_componentes)
         df2 = df2[df2['fecha'] == fecha]
     except KeyError:
         df2 = pd.DataFrame()
